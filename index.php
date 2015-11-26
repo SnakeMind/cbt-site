e7febc953e981841c0bdc88931dad793377031cc
Maurice Post <info@rodeion.nl>
2015-11-26 15:45:26 +0100

    A slightly better placeholder :)

--- index.php
+++ index.php
@@ -1,3 +1,8 @@
-<?php 
-print "Test";
-?>
+<html>
+ <head>
+  <title>CBT learning made easy</title>
+ </head>
+ <body>
+ <?php echo '<p>We have to start somewhere right?</p>'; ?> 
+ </body>
+</html>
