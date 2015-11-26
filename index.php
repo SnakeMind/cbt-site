240251bb7516e2f68dd2453e5b8a8dae5ab6e02a
Maurice Post <info@rodeion.nl>
2015-11-26 16:25:38 +0100

    and some more styling

--- index.php
+++ index.php
@@ -9,6 +9,25 @@
 <link rel="shortcut icon" href="/favicon.ico"/>
 </head>
 <body>
-<?php echo '<p>We have to start somewhere right?</p>'; ?> 
+<header>
+    <p>This should be the header</p>
+</header>
+<nav>
+    <p>Here goes a basic navigation shizzle</p>
+</nav>
+<section>
+    <h1>Here goes introduction and title of the course you are watching mate!</h1>
+<section>
+<arcticle>
+    <header>
+        <h1>You are now watching...</h1>
+    </header>
+    <p>Here goes the actual video bit<br />
+    <?php echo 'We have to start somewhere right?'; ?></p> 
+</article>
+<footer>
+    <p>Created by Maurice Post <&copy;>2015 Rodeion / Ultimum BV</p>
+    <p>Contact me at <a href="mailto:maurice.post@ultimum.nl</a>maurice.post@ultimum.nl</p>
+</footer>
 </body>
 </html>
