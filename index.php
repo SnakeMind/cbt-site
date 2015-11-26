eba550e5dd0c04879891aa38a1ec1bf8770bc08d
Maurice Post <info@rodeion.nl>
2015-11-26 16:13:16 +0100

    meta addition

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 <html>
  <head>
   <title>CBT learning made easy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" media="all" href="/include/master.css" />
+    <meta name="description" content="This is my attempt to build an open-source CBT platform which can be hosted on a local network to ease the way of training your people." />
+    <link rel="shortcut icon" href="/favicon.ico"/>
  </head>
  <body>
  <?php echo '<p>We have to start somewhere right?</p>'; ?> 
