5f3ce4db0ca75d065980ee5091f7d6b0e91ca28f
Maurice Post <info@rodeion.nl>
2015-11-27 11:05:53 +0100

    0.1
    Changes on design, css and a very irritating header example

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <title>CBT learning made easy</title>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" media="all" href="/include/master.css" />
+<link rel="stylesheet" media="all" href="./include/master.css" />
 <meta name="description" content="This is my attempt to build an open-source CBT platform which can be hosted on a local network to ease the way of training your people." />
 <link rel="shortcut icon" href="/favicon.ico"/>
 </head>
